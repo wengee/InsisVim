e094d5a394bfc4c35de934ea954f1cdf5ba89d32
Fung Wing Kit <wengee@qq.com>
2024-04-01 17:50:33 +0800

    update frontend enable

--- init.lua
+++ init.lua
@@ -1,3 +1,27 @@
 require("insis").setup({
   colorscheme = "onedark",
+  frontend = {
+    enable = true,
+    ---@type "eslint" | false
+    linter = "eslint", -- :EslintFixAll command added
+    ---@type false | "prettier" | "tsserver"
+    formatter = "tsserver",
+    format_on_save = true,
+    cspell = false,
+    tailwindcss = true,
+    prisma = false,
+    -- vue will take over typescript lsp
+    vue = true,
+    -- extra lsp command provided by typescript.nvim
+    typescript = {
+      keys = {
+        ts_organize = "gs",
+        ts_rename_file = "gR",
+        ts_add_missing_import = "ga",
+        ts_remove_unused = "gu",
+        ts_fix_all = "gf",
+        ts_goto_source = "gD",
+      },
+    },
+  },
 })
